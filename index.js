0556e1dea487148630e73c1e414c02a77d5eefe3
Vasiliy <vasiliy.mv.obzor@gmail.com>
2017-11-25 19:35:50 +0300

    Cosmetic fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let bot = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('http://ok.ru/');
-    await page.click('#anonymPageContent > div.anonym_login_w > div > div:nth-child(3) > form > div.form-actions > div:nth-child(1) > input');
     await page.waitFor(1000);
     
     let email = "YOUR_EMAIL_OR_MOBILE_NUMBERS";
